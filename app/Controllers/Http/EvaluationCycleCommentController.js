7b2ce0e0cb130e59ed15cc095e172e1afe5ec75b
Mateus Cabral da Silva <kbraltodoteu@gmail.com>
2021-08-12 07:53:38 -0300

    refactor: add check step to avoid errors

--- app/Controllers/Http/EvaluationCycleCommentController.js
+++ app/Controllers/Http/EvaluationCycleCommentController.js
@@ -34,7 +34,10 @@ class EvaluationCycleCommentController {
         await evaluationCycleComment.save();
       });
 
+      const errors = [];
+
       developmentPlans?.forEach(async (plan) => {
+        if (!plan.development_plan_id) errors.push('Preencha o campo Tipo de Plano para todos os PDI`s');
         if (plan.id && plan.id.length > 20) {
           const evaluationCycleDevelopmentPlan = await EvaluationCycleDevelopmentPlan.find(plan.id);
 
@@ -56,6 +59,9 @@ class EvaluationCycleCommentController {
 
       await Redis.del('dashboard-summary');
 
+      if (errors.length > 0) {
+        return response.status(500).json({ message: errors });
+      }
       return response.json({ status: 'ok' });
     } catch (err) {
       console.log(err);
