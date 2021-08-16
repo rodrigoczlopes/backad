2e25aaf3bb9ce8baf710de3ab711e80b77e6a72d
Mateus Cabral da Silva <kbraltodoteu@gmail.com>
2021-08-16 15:25:45 -0300

    feat: add show function

--- app/Controllers/Http/ContinuousFeedbackController.js
+++ app/Controllers/Http/ContinuousFeedbackController.js
@@ -4,6 +4,9 @@
 /** @type {typeof import('@adonisjs/lucid/src/Lucid/Model')} */
 const ContinuousFeedback = use('App/Models/ContinuousFeedback');
 
+/** @type {typeof import('@adonisjs/lucid/src/Lucid/Model')} */
+const ContinuousFeedbackDevelopmentPlan = use('App/Models/ContinuousFeedbackDevelopmentPlan');
+
 class ContinuousFeedbackController {
   async index() {
     return ContinuousFeedback.query()
@@ -13,11 +16,47 @@ class ContinuousFeedbackController {
       .fetch();
   }
 
+  async show({ params }) {
+    const continuousFeedback = await ContinuousFeedback.find(params.id);
+    await continuousFeedback.loadMany({
+      continuousFeedbackDevelopmentPlans: (builder) =>
+        builder.select([
+          'id',
+          'continuous_feedback_id',
+          'employee_id',
+          'leader_id',
+          'action',
+          'initial_date',
+          'final_date',
+          'fake_id',
+        ]),
+      employees: (employee) =>
+        employee
+          .select(['id', 'name', 'admitted_at', 'position_id'])
+          .with('positions', (position) => position.select(['id', 'description'])),
+    });
+    return continuousFeedback;
+  }
+
   async store({ request, response, auth }) {
-    const data = request.all();
+    const { action_plans, ...data } = request.all();
 
     try {
-      return ContinuousFeedback.create({ ...data, created_by: auth.user.id });
+      const continuousFeedback = await ContinuousFeedback.create({ ...data, created_by: auth.user.id });
+
+      if (action_plans && action_plans.length > 0) {
+        action_plans.forEach(async (actionPlan) => {
+          delete actionPlan.id;
+          await ContinuousFeedbackDevelopmentPlan.create({
+            ...actionPlan,
+            continuous_feedback_id: continuousFeedback.id,
+            created_by: auth.user.id,
+            leader_id: auth.user.id,
+          });
+        });
+      }
+
+      return response.status(201).json(continuousFeedback);
     } catch (error) {
       return response.status(500).json({ message: error.message });
     }
