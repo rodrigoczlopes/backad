0e15d5eca9da73f8bbfbefe55972c113bd11fcfb
Mateus Cabral da Silva <kbraltodoteu@gmail.com>
2021-08-27 15:13:56 -0300

    refactor: add new field in loco

--- database/migrations/1630063437656_training_schedule_schema.js
+++ database/migrations/1630063437656_training_schedule_schema.js
@@ -14,6 +14,7 @@ class TrainingScheduleSchema extends Schema {
       table.integer('category');
       table.string('expected_outcome');
       table.uuid('responsible_id').references('id').inTable('users');
+      table.boolean('in_loco');
       table.uuid('created_by').references('id').inTable('users');
       table.uuid('updated_by').references('id').inTable('users');
       table.timestamps();
