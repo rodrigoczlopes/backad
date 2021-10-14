40454b1a0be4be836a155e3b3835ecd111f3b41f
Mateus Cabral da Silva <kbraltodoteu@gmail.com>
2021-10-14 08:15:42 -0300

    refactor: remove commented old code

--- app/Models/Hooks/SendPasswordHook.js
+++ app/Models/Hooks/SendPasswordHook.js
@@ -1,22 +1,3 @@
-// const Kue = use('Kue');
-// const Job = use('App/Jobs/NewPasswordMail');
-
-// class SendPasswordHook {
-//   async sendNewPasswordMail(passwordInstance) {
-//     try {
-//       if (!passwordInstance.dirty.password) return;
-//       const { email, name, username, password } = await passwordInstance;
-
-//       Kue.listen();
-//       Kue.dispatch(Job.key, { email, name, username, password }, { attempts: 3, priority: 'normal', remove: true });
-//     } catch (error) {
-//       return error.message;
-//     }
-//   }
-// }
-
-// module.exports = SendPasswordHook;
-
 const Bull = use('Rocketseat/Bull');
 const Job = use('App/Jobs/NewPasswordMail');
 
